b0cb068b1bf865fb242a7b152eaedd7fa8f3c253
mahendra7041 <chavdamahendra247@gmail.com>
2025-08-26 22:31:42 +0530

    feat: add Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          100: "#e6e8ff",
+          300: "#b2b7ff",
+          400: "#7886d7",
+          500: "#6574cd",
+          600: "#5661b3",
+          800: "#2f365f",
+          900: "#191e38",
+        },
+      },
+      fontFamily: {
+        sans: ['"Cerebri Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
