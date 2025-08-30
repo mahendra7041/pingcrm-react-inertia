80292a13bd8679261d81a19612be99259617a067
mahendra7041 <chavdamahendra247@gmail.com>
2025-08-30 20:26:54 +0530

    refactor: clientStaticBuildDir option removed

--- configs/inertia.config.js
+++ configs/inertia.config.js
@@ -4,10 +4,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const inertiaConfig = {
   rootElementId: "root",
-  clientStaticBuildDir: path.resolve(__dirname, "../build/client"),
   ssrEnabled: true,
   ssrEntrypoint: path.resolve(__dirname, "../src/ssr.jsx"),
-  ssrBuildEntrypoint: path.resolve(__dirname, "../build/ssr/ssr.js"),
+  ssrBuildEntrypoint: path.resolve(__dirname, "../ssr/ssr.js"),
 };
 
 export default inertiaConfig;
