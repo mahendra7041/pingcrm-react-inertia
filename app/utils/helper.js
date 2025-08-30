33f1e22a657b50191c0ea14c8adc488a84fe0377
mahendra7041 <chavdamahendra247@gmail.com>
2025-08-31 00:02:15 +0530

    feat: implemented version hashing function

--- app/utils/helper.js
+++ app/utils/helper.js
@@ -1,3 +1,7 @@
+import { createHash } from "node:crypto";
+import fs from "node:fs";
+import path from "node:path";
+
 export function excludeFields(inputObject, excludedKeys) {
   const result = {};
   Object.keys(inputObject).forEach((key) => {
@@ -6,4 +10,18 @@ export function excludeFields(inputObject, excludedKeys) {
     }
   });
   return result;
+}
+
+export function getManifestHash() {
+  try {
+    if (process.env.NODE_ENV !== "production") return "v1";
+    const manifestFile = fs.readFileSync(
+      path.resolve("public/.vite/manifest.json"),
+      "utf-8"
+    );
+
+    return createHash("md5").update(manifestFile).digest("hex");
+  } catch {
+    return "v1";
+  }
 }
--- configs/inertia.config.js
+++ configs/inertia.config.js
@@ -1,9 +1,13 @@
 import path from "path";
 import { fileURLToPath } from "node:url";
+import { getManifestHash } from "../app/utils/helper.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const assetsVersion = getManifestHash();
 
 const inertiaConfig = {
   rootElementId: "root",
+  assetsVersion,
   ssrEnabled: true,
   ssrEntrypoint: path.resolve(__dirname, "../src/ssr.jsx"),
   ssrBuildEntrypoint: path.resolve(__dirname, "../ssr/ssr.js"),
