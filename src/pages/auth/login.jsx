ae2b3ab104fcb1f586ccefd8556e0eca77f20b67
mahendra7041 <chavdamahendra247@gmail.com>
2025-09-06 11:05:47 +0530

    refactor: login form

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,4 +1,4 @@
-import { Head, useForm } from "@inertiajs/react";
+import { Form, Head } from "@inertiajs/react";
 import Logo from "@/components/Logo";
 import LoadingButton from "@/components/LoadingButton";
 import TextInput from "@/components/TextInput";
@@ -6,17 +6,6 @@ import FieldGroup from "@/components/FieldGroup";
 import CheckboxInput from "@/components/CheckboxInput";
 
 export default function LoginPage() {
-  const { data, setData, errors, post, processing } = useForm({
-    email: "johndoe@example.com",
-    password: "secret",
-    remember: false,
-  });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    post("login");
-  }
-
   return (
     <div className="flex items-center justify-center min-h-screen p-6 bg-indigo-800">
       <Head title="Login" />
@@ -26,59 +15,64 @@ export default function LoginPage() {
           className="block w-full max-w-xs mx-auto text-white fill-current"
           height={50}
         />
-        <form
-          onSubmit={handleSubmit}
+        <Form
+          action="/login"
+          method="post"
           className="mt-8 bg-white rounded-lg shadow-xl overflow-hidden"
         >
-          <div className="px-10 py-12">
-            <h1 className="text-center text-3xl font-bold">Welcome Back!</h1>
-            <div className="mt-6 mx-auto w-24 border-b-2" />
-            <div className="mt-10 space-y-6">
-              <FieldGroup label="Email" name="email" error={errors.email}>
-                <TextInput
-                  name="email"
-                  type="email"
-                  error={errors.email}
-                  value={data.email}
-                  autoFocus={true}
-                  onChange={(e) => setData("email", e.target.value)}
-                />
-              </FieldGroup>
+          {({ errors, processing }) => (
+            <>
+              <div className="px-10 py-12">
+                <h1 className="text-center text-3xl font-bold">
+                  Welcome Back!
+                </h1>
+                <div className="mt-6 mx-auto w-24 border-b-2" />
+                <div className="mt-10 space-y-6">
+                  <FieldGroup label="Email" name="email" error={errors.email}>
+                    <TextInput
+                      name="email"
+                      type="email"
+                      error={errors.email}
+                      defaultValue="johndoe@example.com"
+                      autoFocus={true}
+                    />
+                  </FieldGroup>
 
-              <FieldGroup
-                label="Password"
-                name="password"
-                error={errors.password}
-              >
-                <TextInput
-                  type="password"
-                  error={errors.password}
-                  value={data.password}
-                  onChange={(e) => setData("password", e.target.value)}
-                />
-              </FieldGroup>
+                  <FieldGroup
+                    label="Password"
+                    name="password"
+                    error={errors.password}
+                  >
+                    <TextInput
+                      name="password"
+                      type="password"
+                      error={errors.password}
+                      defaultValue="secret"
+                    />
+                  </FieldGroup>
 
-              <FieldGroup>
-                <CheckboxInput
-                  label="Remember Me"
-                  name="remember"
-                  id="remember"
-                  checked={data.remember}
-                  onChange={(e) => setData("remember", e.target.checked)}
-                />
-              </FieldGroup>
-            </div>
-          </div>
-          <div className="flex items-center justify-end px-10 py-4 bg-gray-100">
-            <LoadingButton
-              type="submit"
-              loading={processing}
-              className="btn-indigo"
-            >
-              Login
-            </LoadingButton>
-          </div>
-        </form>
+                  <FieldGroup>
+                    <CheckboxInput
+                      label="Remember Me"
+                      name="remember"
+                      id="remember"
+                      defaultValue="true"
+                    />
+                  </FieldGroup>
+                </div>
+              </div>
+              <div className="flex items-center justify-end px-10 py-4 bg-gray-100">
+                <LoadingButton
+                  type="submit"
+                  loading={processing}
+                  className="btn-indigo"
+                >
+                  Login
+                </LoadingButton>
+              </div>
+            </>
+          )}
+        </Form>
       </div>
     </div>
   );
